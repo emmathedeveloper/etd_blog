5f4e5cc22f1fe3b75e32f339a84014caf06d64ad
emmathedeveloper <emmathedeveloper@gmail.com>
2026-01-11 18:44:40 +0100

    Fixed bug: undefined initial post

--- src/components/sections/landing/hero.svelte
+++ src/components/sections/landing/hero.svelte
@@ -3,7 +3,7 @@
 	import Background from '../../background.svelte';
 	import { formatDate } from '$lib';
 
-	let { post }: { post: any } = $props();
+	let { post }: { post?: any } = $props();
 </script>
 
 <Background class="relative flex flex-col pb-50">
@@ -20,34 +20,36 @@
 		</div>
 	</header>
 
-	<div class="flex flex-1 flex-col items-center justify-center gap-4">
-		<span class="p-2 font-instrument text-[16px] md:text-[24px]"
-			>latest - {formatDate(post.createdAt)}</span
-		>
-
-		<h1 class="w-full max-w-200 text-center text-[32px] font-extrabold md:text-[64px]">
-			{post.title}
-		</h1>
-
-		<div class="aspect-video h-50 overflow-hidden rounded-4xl md:h-75">
-			<img src={post.poster} alt="hero" class="size-full object-cover" />
-		</div>
-
-		<div class="flex items-center justify-center">
-			<a
-				href={`posts/${post.id}`}
-				class="flex items-center gap-2 rounded-full bg-base px-8 py-2 text-primary"
+	{#if post}
+		<div class="flex flex-1 flex-col items-center justify-center gap-4">
+			<span class="p-2 font-instrument text-[16px] md:text-[24px]"
+				>latest - {formatDate(post.createdAt)}</span
 			>
-				Read Now
-				<ArrowRightIcon />
+
+			<h1 class="w-full max-w-200 text-center text-[32px] font-extrabold md:text-[64px]">
+				{post.title}
+			</h1>
+
+			<div class="aspect-video h-50 overflow-hidden rounded-4xl md:h-75">
+				<img src={post.poster} alt="hero" class="size-full object-cover" />
+			</div>
+
+			<div class="flex items-center justify-center">
+				<a
+					href={`posts/${post.id}`}
+					class="flex items-center gap-2 rounded-full bg-base px-8 py-2 text-primary"
+				>
+					Read Now
+					<ArrowRightIcon />
+				</a>
+			</div>
+
+			<a
+				href="#"
+				class="absolute right-2 bottom-2 flex size-10 animate-bounce items-center justify-center gap-2 rounded-full bg-base text-primary"
+			>
+				<ArrowDownIcon />
 			</a>
 		</div>
-
-		<a
-			href="#"
-			class="absolute right-2 bottom-2 flex size-10 animate-bounce items-center justify-center gap-2 rounded-full bg-base text-primary"
-		>
-			<ArrowDownIcon />
-		</a>
-	</div>
+	{/if}
 </Background>
